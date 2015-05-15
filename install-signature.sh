a885e56d1baf2ea623ce1ba1b44b5b73f78e99e2
Jakob Krigovsky <jakob.krigovsky@gmail.com>
2015-05-15 18:27:36 +0200

    Check if AllSignatures.plist exists before continuing

--- install-signature.sh
+++ install-signature.sh
@@ -4,6 +4,11 @@ set -euo pipefail
 signatureUniqueId="$(uuidgen)"
 signaturesDirectory="$HOME/Library/Mail/V2/MailData/Signatures"
 
+if [ ! -f "$signaturesDirectory/AllSignatures.plist" ]; then
+	>&2 echo "Could not find AllSignatures.plist in $signaturesDirectory."
+	exit 1
+fi
+
 /usr/libexec/PlistBuddy -c "Add :0 dict" "$signaturesDirectory/AllSignatures.plist"
 /usr/libexec/PlistBuddy -c "Add :0:SignatureIsRich bool true" "$signaturesDirectory/AllSignatures.plist"
 /usr/libexec/PlistBuddy -c "Add :0:SignatureName string '$1'" "$signaturesDirectory/AllSignatures.plist"
